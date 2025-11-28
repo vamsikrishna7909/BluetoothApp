460afb51c6d15ff7d52fecb1636fe9b942d056b3
Vamsi Krishna Sivakavi <vamsikrishna7909@gmail.com>
2025-11-28 01:06:56 -0800

    Initial Commit

---
+++ BluetoothApp/BluetoothAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothAppApp.swift
+//  BluetoothApp
+//
+//  Created by Vamsi Krishna Sivakavi on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothAppTests/BluetoothAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothAppTests.swift
+//  BluetoothAppTests
+//
+//  Created by Vamsi Krishna Sivakavi on 11/28/25.
+//
+
+import Testing
+@testable import BluetoothApp
+
+struct BluetoothAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
